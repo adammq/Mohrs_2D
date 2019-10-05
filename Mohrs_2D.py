e50471151e9ac62bffb5f5bd544110c24addbc36
Adam Quaranta <39200635+adammq@users.noreply.github.com>
2019-10-05 09:50:19 -0700

    Update Mohrs_2D.py
    Updated code, making use of Cauchy stress tensor.

--- Mohrs_2D.py
+++ Mohrs_2D.py
@@ -2,79 +2,75 @@ import numpy as np
 from numpy import pi as pi
 from numpy import cos as cos
 from numpy import sin as sin
+from numpy import linalg
 from matplotlib import pyplot as plt
 
 # input units and values
 units = input('units: ')
+
 sigX = float(input("sigma-x: "))
 sigY = float(input("sigma-y: "))
 tauXY = float(input("tau-XY: "))
 
-# calculate average stress and radius
-sig_avg = (sigX + sigY)/2
-R = np.sqrt(((sigX-sigY)/2)**2+tauXY**2)
+#calculate principal normal stresses and absolute maximum shear stress
+Cauchy = np.matrix([[sigX,tauXY],[tauXY,sigY]])
 
-# calculate max and min normal stress and max shear stress
-sig1 = sig_avg + R
-sig2 = sig_avg - R
-tauS = R
+sig_prncpl = linalg.eigvals(Cauchy)
+sig_prncpl = sorted(sig_prncpl,reverse=True)
+tau_abs = abs(sig_prncpl[0]-sig_prncpl[1])/2
 
-# plot Mohr's circle
-a = np.linspace(0,2*pi,256)
+#print value outputs
+print("\nPrincipal normal stresses:\n" +
+	str(round(sig_prncpl[0],2)) + units + "\n" +
+	str(round(sig_prncpl[1],2)) + units + "\n")
+print("Absolute maximum shear stress:\n" +
+	str(round(tau_abs,2))  + units + "\n")
+
+#calculations for plot of Mohr's circle
+#center of circle, corresponding to average in-plane normal stresses
+sig_avg = (sig_prncpl[0] + sig_prncpl[1])/2
+
+#initialize theta
+theta = np.linspace(0,2*pi,256)
+
+#create plot
 plt.figure(figsize=(10,10),dpi=80)
 plt.subplot(1,1,1)
-plt.plot(R*cos(a)+sig_avg,R*sin(a),color="blue",label="X-Y")
 
-# plot initial points
-plt.plot(sigX,tauXY,marker='x',markersize=5,color='black')
-plt.annotate("X "+"("+str(round(sigX,2))+", "+str(round(tauXY,2))+") "+units,
-	xy=(sigX, tauXY), xycoords='data',xytext=(+10, +30),
-	textcoords='offset points', fontsize=8, arrowprops=dict(arrowstyle="->",
-		connectionstyle="arc3,rad=.2"))
-plt.plot(sigY,-tauXY,marker='x',markersize=5,color='black')
-plt.annotate("Y "+"("+str(round(sigY,2))+", "+str(round(-tauXY,2))+") "+units,
-	xy=(sigY, -tauXY), xycoords='data',xytext=(+10, +30),
-	textcoords='offset points', fontsize=8, arrowprops=dict(arrowstyle="->",
-		connectionstyle="arc3,rad=.2"))
+#plot Mohr's circle
+X = tau_abs*cos(theta)+sig_avg
+Y = tau_abs*sin(theta)
+plt.plot(X,Y,color="blue")
 
-#plot important points
-plt.plot(sig_avg,0,marker='x',markersize=5,color='black')
-plt.annotate("C "+"("+str(round(sig_avg,2))+", "+str(round(0.0,2))+") "+units,
-	xy=(sig_avg, 0), xycoords='data',xytext=(+10, +30),
-	textcoords='offset points', fontsize=8, arrowprops=dict(arrowstyle="->",
-		connectionstyle="arc3,rad=.2"))
-plt.plot(sig1,0,marker='x',markersize=5,color='black')
-plt.annotate(r'$\sigma_1 = $'+str(round(sig1,2))+" "+units,xy=(sig1, 0), xycoords='data',xytext=(+10, +30),
-	textcoords='offset points', fontsize=8, arrowprops=dict(arrowstyle="->",
-		connectionstyle="arc3,rad=.2"))
-plt.plot(sig2,0,marker='x',markersize=5,color='black')
-plt.annotate(r'$\sigma_2 = $' + str(round(sig2,2))+" "+units,xy=(sig2, 0), xycoords='data',xytext=(+10, +30),
-	textcoords='offset points', fontsize=8, arrowprops=dict(arrowstyle="->",
-		connectionstyle="arc3,rad=.2"))
-plt.plot(sig_avg,tauS,marker='x',markersize=5,color='black')
-plt.annotate(r'$\tau_S = $' + str(round(tauS,2))+" "+units,xy=(sig_avg, tauS), xycoords='data',xytext=(+10, +30),
-	textcoords='offset points', fontsize=8, arrowprops=dict(arrowstyle="->",
-		connectionstyle="arc3,rad=.2"))
+#notable points on graph
+#absolute maximum shear stress
+plt.plot(sig_avg,tau_abs,marker='o',markersize=6,color="black",
+	label="Absolute maximum shear stress\n" + "(" + str(round(tau_abs,2)) + units + ")")
+#principal normal stresses
+#maximum
+plt.plot(sig_prncpl[0],0,marker='o',markersize=6,color="black",
+	label="Principal normal stresses\n" + "(" + str(round(sig_prncpl[0],2)) + units +
+	", " + str(round(sig_prncpl[1],2)) + units + ")")
+#minimum
+plt.plot(sig_prncpl[1],0,marker='o',markersize=6,color="black")
+#current stress state
+#X-axis
+plt.plot(sigX,tauXY,marker='o',markersize=4,color="red",
+	label="Current stress state\n" + "(" + str(round(sigX,2)) + units +
+	", " + str(round(sigY,2)) + units + ")")
+#Y-axis
+plt.plot(sigY,-tauXY,marker='o',markersize=6,color="red")
+plt.grid(True)
 
-# add reference lines
-plt.plot([sigX,sigY],[tauXY,-tauXY],'r:')
-plt.plot([sig_avg,sig_avg],[0,tauS],'r:')
-
-# add legend
-plt.title("Mohr's Circle")
-#plt.xlabel(r'$\sigma$')
-#plt.ylabel(r'$\tau$')
-plt.legend(loc='upper left')
-
-# modify axes
+#format axes and grid
 ax = plt.gca()  # gca stands for 'get current axis'
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.spines['bottom'].set_position(('data',0))
 ax.yaxis.set_ticks_position('left')
-ax.spines['left'].set_position(('data',0))
+ax.spines['left'].set_position(('data',sig_avg))
 plt.gca().invert_yaxis()
-
-# display graph
+plt.title("Mohr's Circles")
+plt.legend(loc='upper left')
 plt.show()
